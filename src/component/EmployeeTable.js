0520698925e86b53b84420ed14bba6090ae9b3f1
Dj Meechlouch <Meechlouch@gmail.com>
2021-03-02 01:56:36 -0600

    delete btn

--- src/component/EmployeeTable.js
+++ src/component/EmployeeTable.js
@@ -2,28 +2,18 @@ import React from "react";
 
 function EmployeeTable(props) {
   return (
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Position</th>
-          <th>Salary</th>
-          <th>Department</th>
-        </tr>
-      </thead>
-      {props.name.map((item) => {
-        return (
-          <tbody>
-            <tr>
-              <td>{item.name}</td>
-              <td>{item.position}</td>
-              <td>{item.department}</td>
-              <td>{item.salary}</td>
-            </tr>
-          </tbody>
-        );
-      })}
-    </table>
+    <tbody>
+      <tr>
+        <td>{props.name}</td>
+        <td>{props.position}</td>
+        <td>{props.salary}</td>
+        <td>{props.department}</td>
+
+        <button onClick={() => props.removeEmployee(props.id)} className="btn btn-danger">
+          Delete
+        </button>
+      </tr>
+    </tbody>
   );
 }
 export default EmployeeTable;
