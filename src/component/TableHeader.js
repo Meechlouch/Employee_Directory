1f96ee7147166940366ad6241f678c1a73e78175
Dj Meechlouch <Meechlouch@gmail.com>
2021-03-02 01:54:19 -0600

    table header component added

---
+++ src/component/TableHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TableHeader() {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Position</th>
+        <th>Salary</th>
+        <th>Department</th>
+      </tr>
+    </thead>
+  );
+}
+export default TableHeader;
