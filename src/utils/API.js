b51c60950105cae68b9945c8fa3af9beee31e854
Dj Meechlouch <Meechlouch@gmail.com>
2021-03-02 21:20:14 -0600

    API call initialized

---
+++ src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the random user API
+// eslint-disable-next-line
+export default {
+  getEmployees: function () {
+    return axios.get("https://randomuser.me/api/?results=20").then((res) => {
+      const employees = res.data.results;
+      return employees.map((employee) => {
+        return {
+          id: employee.id.value,
+          firstName: employee.name.first,
+          lastName: employee.name.last,
+          email: employee.email,
+          picture: employee.picture.thumbnail,
+        };
+      });
+    });
+  },
+};
